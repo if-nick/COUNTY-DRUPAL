430b940e6bfcc36c09900b6fbb19a06b67e59a10
nickm <nick@ifsight.com>
2022-01-20 17:08:13 -0800

    WCOR-33: Adding search-sort option list.

---
+++ docroot/themes/custom/westy/src/js/_search.js
@@ -0,0 +1,56 @@
+/**
+ * @file Scripting for site-wide headers.
+ */
+
+(function (Drupal) {
+  "use strict";
+
+  /***
+   * Create search sort option.
+   *
+   * Because designs call for a simplified search-sort form that combines
+   * multiple options, we create here in Javascript. That avoids creating more
+   * templates , especially if we probably need Javascript anyway.
+   */
+  Drupal.behaviors.westySearch = {
+    attach: function (context) {
+
+      // Run only once.
+      if (context !== document) return;
+
+      // Check for our empty div in the view header.
+      const container = document.querySelector(
+        "main .view-header div#westy-search-sort-order"
+      );
+
+      if (!container) return;
+
+      // Construct a URL object for inspecting or replacing parameters.
+      let href = new URL(window.location.href);
+      let title_sort = href.searchParams.get("sort_by") === "title";
+
+      // Add our select markup.
+      let selectBox = document.createRange().createContextualFragment(`
+        <select class="form-select form-select-sm" aria-label="form-select-sm">
+          <option value="Title" ${title_sort ? 'selected' : ''}>Sort | A to Z</option>
+          <option value="Relevance" ${title_sort ? '' : 'selected'}>Sort | By relevance</option>
+        </select>`);
+      container.appendChild(selectBox);
+
+      // Check for select-list changes.
+      const select = container.querySelector("select");
+      select.addEventListener("change", function () {
+        let href = new URL(window.location.href);
+        if (this.value.indexOf("Title") > -1) {
+          href.searchParams.set("sort_by", "title");
+          href.searchParams.set("sort_order", "ASC");
+        } else {
+          href.searchParams.set("sort_by", "relevance");
+          href.searchParams.set("sort_order", "DESC");
+        }
+        // Redirect with the new URL parameters.
+        window.location = href;
+      });
+    },
+  };
+})(Drupal);
