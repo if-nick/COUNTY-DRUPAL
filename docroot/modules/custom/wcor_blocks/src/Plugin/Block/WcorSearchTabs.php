61f9f155c27846d59ec3b59d05866071b7d6504f
nickm <nick@ifsight.com>
2022-01-24 15:34:25 -0800

    WCOR-33: Adding custom blocks module. Adding search page tabs block.

---
+++ docroot/modules/custom/wcor_blocks/src/Plugin/Block/WcorSearchTabs.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\wcor_blocks\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Render\Markup;
+
+/**
+ * Provides a custom block.
+ *
+ * @Block(
+ *   id = "wcor_blocks__search_page_tabs",
+ *   admin_label = @Translation("WCOR Search Page Tabs"),
+ *   category = @Translation("WCOR"),
+ * )
+ */
+class WcorSearchTabs extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+
+    return [
+      'label_display' => FALSE,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $current_path = \Drupal::service('path.current')->getPath();
+
+    // @TODO: Add tab links, once available.
+    $html = '
+  <ul class="nav">
+    <li class="nav-item">
+      <h3><a class="nav-link' . ($current_path === '/search' ? ' active' : '') . '" href="#">All</a></h3>
+    </li>
+    <li class="nav-item">
+      <h3><a class="nav-link" href="#">News</a></h3>
+    </li>
+    <li class="nav-item">
+      <h3><a class="nav-link" href="#">Service</a></h3>
+    </li>
+    <li class="nav-item">
+      <h3><a class="nav-link" href="#">Document</a></h3>
+    </li>
+  </ul>
+';
+
+    return [
+      'div' => [
+        '#type' => 'html_tag',
+        '#tag' => 'div',
+        '#id' => 'wcor-search-page-tabs',
+        '#attributes' => [
+          'class' => ['wcor-blocks'],
+          'id' => 'wcor-search-page-tabs',
+        ],
+        '#children' => $html,
+      ],
+      '#attached' => [
+        'library' => [],
+      ],
+    ];
+  }
+
+}
